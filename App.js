3be023e44c63d6050d68bba0705975666bf3e23c
sershishkov <ser.shishkov@gmail.com>
2019-09-28 19:55:32 +0300

    8-3-add2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>New App!!!</Text>
     </View>
   );
